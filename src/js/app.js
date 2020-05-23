565db7ab30434f48e894970b3baa9c5918fe1c73
Ben Lam <ben.lam@haymarket.asia>
2020-05-23 19:01:33 +0800

    Add tag coloring script

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 import Zooming from 'zooming'
+import md5 from 'js-md5'
+
 const $ = window.jQuery
 require('slick-carousel')
 
@@ -21,4 +23,36 @@ $(() => {
       }
     ]
   })
+})
+
+$(() => {
+  // console.debug(md5('hello, world'))
+  $('article a.c-tag').each(function () {
+    var hash = md5($(this).text())
+    var h = hash.split('').map(c => c.charCodeAt(0)).reduce((acc, cur) => acc + cur) % 360
+    $(this).attr('data-hue', h)
+  })
+
+  // $('.c-tag').each(function () {
+  //   const hue = $(this).data('hue')
+  //   set_color($(this), hue, 75, 90)
+  // })
+
+  $('.c-tag').on('mouseover', function () {
+    const hue = $(this).data('hue')
+    setColor($(this), hue, 30)
+  })
+
+  $('.c-tag').on('mouseleave', function () {
+    const hue = $(this).data('hue')
+    setColor($(this), hue, 75)
+  })
+
+  $('.c-tag').trigger('mouseleave')
+
+  function setColor ($target, h, l) {
+    console.debug(`hsl(${h}, 85%, ${l}%)`)
+    $target.css('color', `hsl(${h}, 85%, ${l}%)`)
+    // $target.css('background-color', `hsl(${h + (360 / 2)}, 85%, ${bgl}%)`)
+  }
 })
